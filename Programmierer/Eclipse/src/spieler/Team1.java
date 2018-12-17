c038774965c07e00f442fcffc8d955eafa595f11
HexadimensionalerAlp <38053277+HexadimensionalerAlp@users.noreply.github.com>
2018-12-17 18:30:24 +0100

    Wrapper + modularisierung

---
+++ Programmierer/Eclipse/src/spieler/Team1.java
@@ -0,0 +1,61 @@
+package spieler;
+
+public class Team1 {
+
+    void firstPlayer() {
+
+    }
+
+    void secondPlayer() {
+
+    }
+
+    boolean isItOver() {
+	return false;
+    }
+
+    String andTheWinnerIs() {
+	return null;
+    }
+
+    // Bewegung
+
+    boolean canPlayerMove(String player) {
+	return false;
+    }
+
+    boolean isValidMove(String cord1, String cord2, String xClass) {
+	return false;
+    }
+
+    String move(String cord1, String cord2) {
+	return null;
+    }
+
+    String[] getNextMove() {
+	return null;
+    }
+
+    // Angriff
+
+    boolean canPlayerAttack(String Player) {
+	return false;
+    }
+
+    boolean isValidAttack(String cord1, String cord2, String xClass) {
+	return false;
+    }
+
+    String attack(String cord1, String cord2, String attackType) {
+	return null;
+    }
+
+    String[] getNextAttack() {
+	return null;
+    }
+
+    String whoIsTheLooser(String ko1, String ko2) {
+	return null;
+    }
+
+}
